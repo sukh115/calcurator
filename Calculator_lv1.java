e456d662bb1dfd26722d9623fd2a1cbfde7d14ba
sukh115 <sukh1151102@gmail.com>
2025-02-27 19:02:03 +0900

    lv1 수정

--- Calculator_lv1.java
+++ Calculator_lv1.java
@@ -4,18 +4,19 @@ public class Calculator_lv1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+
+        while (true) {
+
             System.out.println("양의 정수를 입력해주세요 또는 'exit'입력시 종료 :");
             String input = sc.nextLine();
-            if (input.equalsIgnoreCase("exit")) {
-                System.out.println("계산기를 종료합니다.");
-                return;
+            if (input.equals("exit")) {
+                break;
             }
             int a = Integer.parseInt(input);
-        while (true) {
+
             System.out.println("연산자를 입력하세요 (+,-,*,/) 또는 'exit'입력시 종료 :");
             String input2 = sc.nextLine();
-            if (input2.equalsIgnoreCase("exit")) {
-                System.out.println("계산기를 종료합니다.");
+            if (input2.equals("exit")) {
                 break;
             }
 
@@ -23,8 +24,7 @@ public class Calculator_lv1 {
 
             System.out.println("다음 양의 정수를 입력해주세요 또는 'exit'입력시 종료 :");
             String input3 = sc.nextLine();
-            if (input3.equalsIgnoreCase("exit")) {
-                System.out.println("계산기를 종료합니다.");
+            if (input3.equals("exit")) {
                 break;
             }
 
@@ -55,5 +55,13 @@ public class Calculator_lv1 {
             System.out.println("결과 값 : " + a);
         }
         sc.close();
+    }
+
+    public static boolean exit(String input) {
+        if (input.equalsIgnoreCase("exit")) { // exit 체크
+            System.out.println("계산기를 종료합니다.");
+            return true; // exit 입력 시 true 반환
+        }
+        return false;
     }
 }
